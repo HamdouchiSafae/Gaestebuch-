bd780833fc91a0a44145ba4e51ad24b3321c3b06
HamdouchiSafae <Safae.Hamdouchi@Student.HTW-Berlin.de>
2023-10-26 20:59:05 +0200

    Initial Commit

---
+++ src/test/java/de/htwberlin/gaestebuch/GaestebuchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.gaestebuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaestebuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
